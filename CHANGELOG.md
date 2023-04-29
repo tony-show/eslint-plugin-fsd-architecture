79cd3db9876da094dc763dcf2c07acafa276debf
Anatoly Ivashov <tonyshow2011@gmail.com>
2023-04-29 11:20:58 +0200

    Add auto fix for public-api-imports rule
    If the --fix option on the command line automatically fixes problems of "Absolute import is allowed only from public API (index.js/ts)" reported by the rule.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+#### 0.0.12 (2023-04-29)
+* Add auto fix option for public-api-imports rule
+If the --fix option on the command line automatically fixes problems of "Absolute import is allowed only from public API (index.js/ts)" reported by the rule.
 #### 0.0.11 (2023-04-27)
 * Add git repo url
 #### 0.0.10 (2023-04-27)
--- tests/lib/rules/public-api-imports.js
+++ tests/lib/rules/public-api-imports.js
@@ -8,7 +8,8 @@
 //------------------------------------------------------------------------------
 
 const rule = require("../../../lib/rules/public-api-imports"),
-  RuleTester = require("eslint").RuleTester;
+RuleTester = require("eslint").RuleTester;
+const { PUBLIC_ERROR, TESTING_PUBLIC_ERROR } = require('../../../lib/consts/public-api-imports.js')
 
 
 //------------------------------------------------------------------------------
@@ -19,22 +20,18 @@ const ruleTester = new RuleTester({
   parserOptions: {ecmaVersion: 6, sourceType: 'module'}
 });
 
-const aliasOptions = [
-  {
-    alias: '@'
-  }
-]
-
 ruleTester.run("public-api-imports", rule, {
   valid: [
     {
       code: "import { ArticleCommentSlice } from '../../model/slices/addCommentFormSlice'",
-      errors: []
     },
     {
       code: "import { ArticleCommentSlice } from '@/entities/Article'",
-      errors: [],
-      options: aliasOptions
+      options: [
+        {
+          alias: '@'
+        }
+      ]
     },
     {
       filename: 'C:/Users/project/src/entities/Article/file.test.ts',
@@ -61,30 +58,39 @@ ruleTester.run("public-api-imports", rule, {
   invalid: [
     {
       code: "import { ArticleCommentSlice } from '@/entities/Article/model/file.ts'",
-      errors: [{ message: "Absolute import is allowed only from public API (index.js/ts)" }],
-      options: aliasOptions
+      errors: [{ messageId: PUBLIC_ERROR }],
+      options: [
+        {
+          alias: '@'
+        }
+      ],
+      output: `import { ArticleCommentSlice } from '@/entities/Article'`
     },
     {
       filename: 'C:/Users/project/src/entities/Article/StoreDecorator.tsx',
       code: "import { ArticleCommentSlice } from '@/entities/Article/testing/file.tsx'",
-      errors: [{ message: "Absolute import is allowed only from public API (index.js/ts)" }],
+      errors: [{ messageId: PUBLIC_ERROR }],
       options: [
         {
           alias: '@',
           testFilesPatterns: ['**/*.test.*', '**/*.stories.*', '**/StoreDecorator.tsx'],
         }
-      ]
+      ],
+      output: `import { ArticleCommentSlice } from '@/entities/Article'`
     },
     {
       filename: 'C:/Users/project/src/entities/Article/forbidden.ts',
       code: "import { ArticleCommentSlice } from '@/entities/Article/testing'",
-      errors: [{ message: "Test data need import from public API file for tests (testing.js/ts) only in files from testFilesPatterns option" }],
+      errors: [{
+        messageId: TESTING_PUBLIC_ERROR
+      }],
       options: [
         {
           alias: '@',
           testFilesPatterns: ['**/*.test.*', '**/*.stories.*', '**/StoreDecorator.tsx'],
         }
-      ]
+      ],
+      output: null
     }
   ],
 });
